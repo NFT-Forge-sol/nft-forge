43590535017dde5da61d4cde5b65c6bc55e4cabe
MatheoKatbie <matheo.okatbie@icloud.com>
2024-12-19 20:55:30 +0100

    add range for nft and select for style and background

--- forge-app/src/App.jsx
+++ forge-app/src/App.jsx
@@ -8,7 +8,21 @@ import WalletContextProvider from './Components/WalletContextProvider'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [range, setRange] = useState(100)
+
+  const handleRangeChange = (event) => {
+    setRange(event.target.value)
+  }
+
+  const handleBlur = (event) => {
+    if (event.target.value < 100) {
+      setRange(100)
+    }
+
+    if (event.target.value > 1000) {
+      setRange(1000)
+    }
+  }
 
   return (
     <>
@@ -22,12 +36,29 @@ function App() {
           </a>
         </div>
         <h1>Vite + React</h1>
-        <WalletDisplay />
         <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
+          <div>
+            <input type="range" min={100} max={1000} value={range} step={100} onChange={handleRangeChange} />
+            <label>Number of NFTs: </label>
+            <input type="number" min={100} max={1000} value={range} onChange={handleRangeChange} onBlur={handleBlur} />
+          </div>
+          <div>
+            <label>Style</label>
+            <select>
+              <option>Drawing</option>
+              <option>8-Bit</option>
+              <option>Real</option>
+            </select>
+          </div>
+          <div>
+            <label>Background</label>
+            <select>
+              <option>Full Color</option>
+              <option>Fade</option>
+              <option>City</option>
+            </select>
+          </div>
+          <WalletDisplay />
           <WalletMultiButton />
         </div>
         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
